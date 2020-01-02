556342a1b4ef0e2ef8c3309fa0c00588895f0b51
Valery-Bond <52550003+Valery-Bond@users.noreply.github.com>
2020-01-02 02:36:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
    
-    info.addEventListener('click', function(event){
+    info.addEventListener('click', function (event){
         let target = event.target;
         if (target && target.classList.contains("info-header-tab")) {
             for (let i = 0; i < tab.length; i++) {
